41a422ec12717c0cb7ad21109a41e40c9dad929a
Oleksandr Korniienko <oleksanderkorn@gmail.com>
2021-11-16 11:26:07 +0100

    Arbitrage Scanner

--- src/main/java/com/lkskrn/arbitrage/ws/WebSocketConfig.java
+++ src/main/java/com/lkskrn/arbitrage/ws/WebSocketConfig.java
@@ -20,6 +20,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
         webSocketHandlerRegistry.addHandler(websocketHandler, "/prices").setAllowedOrigins(
-                "https://arbitrage-ui.herokuapp.com/", "https://oleksanderkorn.github.io/arbitrage-scanner-ui/");
+                "https://arbitrage-ui.herokuapp.com/", "https://oleksanderkorn.github.io/arbitrage-scanner-ui/",
+                "http://localhost:3000");
     }
 }
